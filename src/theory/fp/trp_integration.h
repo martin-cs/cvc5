7ccebaca7e450b10c15a3faa598e74a080e877ee
martin <martin@raphael>
2013-10-16 13:00:09 +0100

    A custom branching heuristic for CVC4/TRP.

--- src/theory/fp/trp_integration.h
+++ src/theory/fp/trp_integration.h
@@ -20,6 +20,7 @@
 #include "trp/algorithms/probeDecisionLearning.h"
 
 
+
 namespace CVC4 {
 namespace theory {
 namespace fp {
@@ -54,10 +55,108 @@ namespace fp {
 
   typedef TRP::meet<AbstractElementWithTrail, ClauseDB, Expressions> Transformer;
 
+}
+}
+}
+
+
+namespace TRP {
+  template <class H>
+    struct largestUnassignedVariableFirst {
+      
+    protected :
+      H base;
+      typedef CVC4::theory::fp::AbstractElement SAbs;
+      typedef CVC4::FloatingPointLiteral Number;
+
+      void scoreInterval(const interval< Number > &i, Number &score) {
+	ARGUMENTERROR(i.isComplete() == downwardCompleteness::Incomplete);
+
+	Number low(i.lower);
+	if (low.isInfinite()) {
+	  DYNAMICASSERT(low == Number(-INFINITY));  // As incomplete
+	  low.inc();
+	}
+
+	score = i.upper;
+	if (score.isInfinite()) {
+	  DYNAMICASSERT(score == Number(+INFINITY));  // As incomplete
+	  score.dec();
+	}
+
+	Number half(0.5);
+	low.mul(FE_TONEAREST,half);
+	score.mul(FE_TONEAREST,half);
+
+	score.sub(FE_TONEAREST,low);
+
+	return;
+      }
+
+
+    public :
+      meetIrreducible<SAbs> extrapolate (const SAbs &astate) {
+ 
+	downwardCompleteness result(downwardCompleteness::Unknown);
+	bool uninitialised = true;
+	SAbs::keyType currentLargest;
+	Number current;
+	Number candidate;
+
+	TRACE("largestUnassignedVariableFirst") << "extrapolate" << std::endl;
+	DYNAMICASSERT(astate.isComplete() == downwardCompleteness::Incomplete);
+
+	for (SAbs::constAbsIterator i = astate.begin(); i != astate.end(); ++i) {
+	  unsigned k = (*i).first.getKind();
+
+	  if (((k == CVC4::kind::VARIABLE) ||
+	       (k == CVC4::kind::BOUND_VARIABLE)) &&
+	      ((*i).second.isComplete() == downwardCompleteness::Incomplete)) {
+
+	    if (((const proposition &)((*i).second)).isComplete() == downwardCompleteness::Incomplete) {
+		// Can be set to NaN, thus finish the search
+	      currentLargest = (*i).first;
+	      uninitialised = true;
+	      break;
+	    }
+
+
+	    if (uninitialised) {
+	      uninitialised = false;
+	      currentLargest = (*i).first;
+	      scoreInterval((*i).second, current);
+
+	    } else {
+	      scoreInterval((*i).second, candidate);
+	      if (candidate > current) {
+		currentLargest = (*i).first;
+		current.swap(candidate);
+	      }
+	    }
+	  }
+
+	}
+
+	DYNAMICASSERT(uninitialised == false);
+	
+	return meetIrreducible<SAbs>(currentLargest,
+				     base.extrapolate(astate[currentLargest]));
+ 
+      }
+    };
+}
+
+
+namespace CVC4 {
+namespace theory {
+namespace fp {
 
 
   typedef TRP::firstUnassigned< FloatingPointInterval, TRP::typePair<TRP::split<TRP::proposition>, TRP::split<TRP::interval< CVC4::FloatingPointLiteral > > > >  NANFirstThenSplit;
   typedef TRP::firstUnassigned< AbstractElement, NANFirstThenSplit > Heuristic;
+
+  // New branching heuristic
+  //typedef TRP::largestUnassignedVariableFirst< NANFirstThenSplit > Heuristic;
 
   typedef TRP::probeDecisionLearning<AbstractElementWithTrail, Transformer, Heuristic> Solver;
 
